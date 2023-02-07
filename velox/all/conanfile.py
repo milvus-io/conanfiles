4a7108545d7002fca3903b0e96ae31d5730d8110
Enwei Jiao <enwei.jiao@zilliz.com>
2023-02-07 09:52:38 +0800

    init

---
+++ velox/all/conanfile.py
@@ -0,0 +1,243 @@
+from conan.tools.microsoft import is_msvc, msvc_runtime_flag
+from conan.tools.build import can_run, check_min_cppstd
+from conan.tools.scm import Version
+from conan.tools import files
+from conan import ConanFile
+from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
+from conan.errors import ConanInvalidConfiguration
+from conans import tools
+import os
+
+required_conan_version = ">=1.45.0"
+
+
+class VeloxConan(ConanFile):
+    name = "velox"
+    description = ""
+    topics = ("facebook", "engine", "simd")
+    url = "https://github.com/facebookincubator/velox"
+    homepage = "https://github.com/facebookincubator/velox"
+    license = "Apache-2.0"
+
+    settings = "os", "arch", "compiler", "build_type"
+    options = {
+        "shared": [True, False],
+        "fPIC": [True, False],
+    }
+    default_options = {"shared": False, "fPIC": True}
+    version = "2023.01.19.00"
+
+    @property
+    def _source_subfolder(self):
+        return "source_subfolder"
+
+    @property
+    def _minimum_cpp_standard(self):
+        return 17
+
+    @property
+    def _minimum_compilers_version(self):
+        return {
+            "gcc": "8",
+            "Visual Studio": "16",
+            "clang": "6",
+            "apple-clang": "10",
+        }
+
+    def export_sources(self):
+        self.copy("CMakeLists.txt")
+        for patch in self.conan_data.get("patches", {}).get(self.version, []):
+            self.copy(patch["patch_file"])
+
+    def config_options(self):
+        if self.settings.os == "Windows":
+            try:
+                del self.options.fPIC
+            except Exception:
+                pass
+
+    def configure(self):
+        if self.options.shared:
+            try:
+                del self.options.fPIC
+            except Exception:
+                pass
+
+    def requirements(self):
+        self.requires("folly/2022.10.31.00@milvus/dev")
+        self.requires("boost/1.80.0")
+        self.requires("bzip2/1.0.8")
+        self.requires("double-conversion/3.2.1")
+        self.requires("xsimd/9.0.1")
+        self.requires("gtest/1.8.1")
+        self.requires("gflags/2.2.2")
+        self.requires("glog/0.6.0")
+        self.requires("libevent/2.1.12")
+        self.requires("lz4/1.9.4")
+        self.requires("lzo/2.10")
+        self.requires("snappy/1.1.9")
+        self.requires("zlib/1.2.13")
+        self.requires("zstd/1.5.2")
+        self.requires("libsodium/cci.20220430")
+        self.requires("openssl/1.1.1q")
+        self.requires("xz_utils/5.2.5")
+        self.requires("protobuf/3.21.9")
+
+    @property
+    def _required_boost_components(self):
+        return ["context", "filesystem", "program_options", "regex", "system", "thread"]
+
+    def validate(self):
+        if self.settings.compiler.get_safe("cppstd"):
+            check_min_cppstd(self, self._minimum_cpp_standard)
+        min_version = self._minimum_compilers_version.get(str(self.settings.compiler))
+        if not min_version:
+            self.output.warn(
+                "{} recipe lacks information about the {} compiler support.".format(
+                    self.name, self.settings.compiler
+                )
+            )
+        else:
+            if Version(self.settings.compiler.version) < min_version:
+                raise ConanInvalidConfiguration(
+                    "{} requires C++{} support. The current compiler {} {} does not support it.".format(
+                        self.name,
+                        self._minimum_cpp_standard,
+                        self.settings.compiler,
+                        self.settings.compiler.version,
+                    )
+                )
+
+        min_version = self._minimum_compilers_version.get(str(self.settings.compiler))
+        if not min_version:
+            self.output.warn(
+                "{} recipe lacks information about the {} compiler support.".format(
+                    self.name, self.settings.compiler
+                )
+            )
+        else:
+            if Version(self.settings.compiler.version) < min_version:
+                raise ConanInvalidConfiguration(
+                    "{} requires C++{} support. The current compiler {} {} does not support it.".format(
+                        self.name,
+                        self._minimum_cpp_standard,
+                        self.settings.compiler,
+                        self.settings.compiler.version,
+                    )
+                )
+
+    def source(self):
+        files.get(
+            self,
+            **self.conan_data["sources"][self.version],
+            destination=self._source_subfolder,
+            strip_root=True,
+        )
+
+    def layout(self):
+        cmake_layout(self)
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.variables["CMAKE_POSITION_INDEPENDENT_CODE"] = self.options.get_safe(
+            "fPIC", True
+        )
+        # Relocatable shared lib on Macos
+        tc.cache_variables["CMAKE_POLICY_DEFAULT_CMP0042"] = "NEW"
+        # Honor BUILD_SHARED_LIBS from conan_toolchain (see https://github.com/conan-io/conan/issues/11840)
+        tc.cache_variables["CMAKE_POLICY_DEFAULT_CMP0077"] = "NEW"
+
+        cxx_std_flag = tools.cppstd_flag(self.settings)
+        cxx_std_value = (
+            cxx_std_flag.split("=")[1]
+            if cxx_std_flag
+            else "c++{}".format(self._minimum_cpp_standard)
+        )
+        tc.variables["CXX_STD"] = cxx_std_value
+        if is_msvc(self):
+            tc.variables["MSVC_LANGUAGE_VERSION"] = cxx_std_value
+            tc.variables["MSVC_ENABLE_ALL_WARNINGS"] = False
+            tc.variables["MSVC_USE_STATIC_RUNTIME"] = "MT" in msvc_runtime_flag(self)
+        tc.variables["VELOX_BUILD_TESTING"] = False
+
+        tc.generate()
+
+        deps = CMakeDeps(self)
+        deps.generate()
+
+    def build(self):
+        files.apply_conandata_patches(self)
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def package(self):
+        self.copy(pattern="LICENSE", dst="licenses", src=self._source_subfolder)
+        cmake = CMake(self)
+        cmake.install()
+        files.rmdir(self, os.path.join(self.package_folder, "lib", "cmake"))
+        files.rmdir(self, os.path.join(self.package_folder, "lib", "pkgconfig"))
+
+    def package_info(self):
+        self.cpp_info.set_property("cmake_file_name", "velox")
+        self.cpp_info.set_property("cmake_target_name", "Velox::velox_bundled")
+        self.cpp_info.set_property("pkg_config_name", "libvelox")
+
+        self.cpp_info.components["libvelox"].libs = [
+            "velox_bundled",
+            "velox_test_bundled",
+        ]
+
+        self.cpp_info.components["libvelox"].requires = [
+            "boost::context",
+            "boost::filesystem",
+            "boost::program_options",
+            "boost::regex",
+            "boost::system",
+            "boost::thread",
+            "bzip2::bzip2",
+            "double-conversion::double-conversion",
+            "gflags::gflags",
+            "glog::glog",
+            "libevent::libevent",
+            "lz4::lz4",
+            "lzo::lzo",
+            "snappy::snappy",
+            "zlib::zlib",
+            "zstd::zstd",
+            "libsodium::libsodium",
+            "openssl::openssl",
+            "xz_utils::xz_utils",
+            "Folly::folly",
+        ]
+        if (
+            self.settings.os == "Linux"
+            and self.settings.compiler == "clang"
+            and self.settings.compiler.libcxx == "libstdc++"
+        ):
+            self.cpp_info.components["libvelox"].system_libs.append("atomic")
+
+        if (
+            self.settings.os == "Macos"
+            and self.settings.compiler == "apple-clang"
+            and Version(self.settings.compiler.version.value) >= "11.0"
+        ) or (
+            self.settings.os == "Macos"
+            and self.settings.compiler == "clang"
+            and Version(self.settings.compiler.version.value) >= "11.0"
+        ):
+            self.cpp_info.components["libfolly"].system_libs.append("c++abi")
+
+        # TODO: to remove in conan v2 once cmake_find_package_* & pkg_config generators removed
+        self.cpp_info.filenames["cmake_find_package"] = "velox"
+        self.cpp_info.filenames["cmake_find_package_multi"] = "velox"
+        self.cpp_info.names["cmake_find_package"] = "Velox"
+        self.cpp_info.names["cmake_find_package_multi"] = "Velox"
+        self.cpp_info.names["pkg_config"] = "libvelox"
+        self.cpp_info.components["libvelox"].names["cmake_find_package"] = "velox"
+        self.cpp_info.components["libvelox"].names["cmake_find_package_multi"] = "velox"
+
+        self.cpp_info.components["libvelox"].set_property(
+            "cmake_target_name", "Velox::velox"
+        )
+        self.cpp_info.components["libvelox"].set_property("pkg_config_name", "libvelox")
