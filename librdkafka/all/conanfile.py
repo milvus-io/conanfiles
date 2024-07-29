0da7aa501966b944b37b86fd4b51b053017560ca
Yellow Shine <sammy.huang@zilliz.com>
2024-07-29 20:40:50 +0800

    librdkafka
    Signed-off-by: Yellow Shine <sammy.huang@zilliz.com>

--- librdkafka/all/conanfile.py
+++ librdkafka/all/conanfile.py
@@ -3,6 +3,7 @@ from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
 from conan.tools.env import VirtualBuildEnv
 from conan.tools.files import apply_conandata_patches, copy, export_conandata_patches, get, rmdir
 from conan.tools.gnu import PkgConfigDeps
+from conan.tools.scm import Version
 import os
 
 required_conan_version = ">=1.55.0"
@@ -51,6 +52,8 @@ class LibrdkafkaConan(ConanFile):
     def config_options(self):
         if self.settings.os == "Windows":
             del self.options.fPIC
+        if Version(self.version) < "1.9.0":
+            del self.options.curl
 
     def configure(self):
         if self.options.shared:
@@ -68,13 +71,13 @@ class LibrdkafkaConan(ConanFile):
         if self.options.ssl:
             self.requires("openssl/[>=1.1 <4]")
         if self._depends_on_cyrus_sasl:
-            self.requires("cyrus-sasl/2.1.28")
-        if self.options.curl:
-            self.requires("libcurl/[>=7.78.0 <9]")
+            self.requires("cyrus-sasl/2.1.27")
+        if self.options.get_safe("curl", False):
+            self.requires("libcurl/8.0.1")
 
     def build_requirements(self):
         if self._depends_on_cyrus_sasl:
-            self.tool_requires("pkgconf/2.1.0")
+            self.tool_requires("pkgconf/1.9.3")
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
@@ -97,7 +100,8 @@ class LibrdkafkaConan(ConanFile):
         tc.variables["WITH_SSL"] = self.options.ssl
         tc.variables["WITH_SASL"] = self.options.sasl
         tc.variables["ENABLE_LZ4_EXT"] = True
-        tc.variables["WITH_CURL"] = self.options.curl
+        if Version(self.version) >= "1.9.0":
+            tc.variables["WITH_CURL"] = self.options.curl
         tc.generate()
 
         cd = CMakeDeps(self)
