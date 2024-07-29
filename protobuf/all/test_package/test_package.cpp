b4051ef3577a502f333eeea71b00508962730fb0
Yellow Shine <sammy.huang@zilliz.com>
2024-07-29 19:29:45 +0800

    protobuf
    Signed-off-by: Yellow Shine <sammy.huang@zilliz.com>

--- protobuf/all/test_package/test_package.cpp
+++ protobuf/all/test_package/test_package.cpp
@@ -1,15 +1,17 @@
 #include <cstdlib>
 #include <iostream>
 
-#include <google/protobuf/timestamp.pb.h>
-#include <google/protobuf/util/time_util.h>
+#include "addressbook.pb.h"
 
 int main()
 {
-	google::protobuf::Timestamp ts;
-	google::protobuf::util::TimeUtil::FromString("1972-01-01T10:00:20.021Z", &ts);
-	const auto nanoseconds = ts.nanos();
+	std::cout << "Bincrafters\n";
 
-	std::cout << "1972-01-01T10:00:20.021Z in nanoseconds: " << nanoseconds << "\n";
+	tutorial::Person p;
+	p.set_id(21);
+	p.set_name("conan-center-index");
+	p.set_email("info@conan.io");
+
+	std::cout << p.SerializeAsString() << "\n";
 	return EXIT_SUCCESS;
 }
