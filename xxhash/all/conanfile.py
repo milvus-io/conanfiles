ba6083600db343bb63986431186a198b9c1fd83c
cqy123456 <qianya.cheng@zilliz.com>
2025-05-29 08:40:56 -0500

    update xxhash package
    Signed-off-by: cqy123456 <qianya.cheng@zilliz.com>

--- xxhash/all/conanfile.py
+++ xxhash/all/conanfile.py
@@ -1,37 +1,31 @@
 from conan import ConanFile
-from conan.errors import ConanInvalidConfiguration
-from conan.tools.env import VirtualBuildEnv
-from conan.tools.files import get, copy, rmdir, export_conandata_patches, apply_conandata_patches
-from conan.tools.gnu import Autotools, AutotoolsToolchain
-from conan.tools.layout import basic_layout
-from conan.tools.microsoft import is_msvc
+from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
+from conan.tools.files import apply_conandata_patches, copy, export_conandata_patches, get, rmdir
 from conan.tools.scm import Version
 import os
 
-required_conan_version = ">=1.54.0"
+required_conan_version = ">=1.53.0"
 
-class XXHashConan(ConanFile):
+
+class XxHashConan(ConanFile):
     name = "xxhash"
     description = "Extremely fast non-cryptographic hash algorithm"
     license = "BSD-2-Clause"
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://github.com/Cyan4973/xxHash"
-    topics = ("hash", "algorithm", "fast", "checksum")
+    topics = ("hash", "algorithm", "fast", "checksum", "hash-functions")
+    package_type = "library"
     settings = "os", "arch", "compiler", "build_type"
     options = {
         "shared": [True, False],
         "fPIC": [True, False],
-        "utility": [True, False],  # Whether to build the xxhsum utility
+        "utility": [True, False],
     }
     default_options = {
         "shared": False,
         "fPIC": True,
         "utility": True,
     }
-
-    @property
-    def _settings_build(self):
-        return getattr(self, "settings_build", self.settings)
 
     def export_sources(self):
         export_conandata_patches(self)
@@ -43,65 +37,45 @@ class XXHashConan(ConanFile):
     def configure(self):
         if self.options.shared:
             self.options.rm_safe("fPIC")
-        self.settings.rm_safe("compiler.libcxx")
         self.settings.rm_safe("compiler.cppstd")
+        self.settings.rm_safe("compiler.libcxx")
 
     def layout(self):
-        basic_layout(self, src_folder="src")
-
-    def build_requirements(self):
-        if self._settings_build.os == "Windows":
-            self.win_bash = True
-            if not self.conf.get("tools.microsoft.bash:path", check_type=str):
-                self.tool_requires("msys2/cci.latest")
+        cmake_layout(self, src_folder="src")
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
 
     def generate(self):
-        env = VirtualBuildEnv(self)
-        env.generate()
-        tc = AutotoolsToolchain(self)
-        if not self.options.utility:
-            tc.configure_args.append("--disable-cli")
-        if is_msvc(self):
-            tc.extra_cflags.append("-nologo")
-            tc.extra_ldflags.append("-nologo")
+        tc = CMakeToolchain(self)
+        tc.variables["XXHASH_BUNDLED_MODE"] = False
+        tc.variables["XXHASH_BUILD_XXHSUM"] = self.options.utility
+        # Fix CMake configuration if target is iOS/tvOS/watchOS
+        tc.cache_variables["CMAKE_MACOSX_BUNDLE"] = False
+        # Generate a relocatable shared lib on Macos
+        tc.cache_variables["CMAKE_POLICY_DEFAULT_CMP0042"] = "NEW"
+        if Version(self.version) < "0.8.3":
+            tc.cache_variables["CMAKE_POLICY_VERSION_MINIMUM"] = "3.5" # CMake 4 support
         tc.generate()
 
     def build(self):
         apply_conandata_patches(self)
-        autotools = Autotools(self)
-        autotools.configure()
-        autotools.make()
+        cmake = CMake(self)
+        cmake.configure(build_script_folder=os.path.join(self.source_folder, "cmake_unofficial"))
+        cmake.build()
 
     def package(self):
         copy(self, "LICENSE", src=self.source_folder, dst=os.path.join(self.package_folder, "licenses"))
-        autotools = Autotools(self)
-        autotools.install()
-        
-        rmdir(self, os.path.join(self.package_folder, "share"))
+        cmake = CMake(self)
+        cmake.install()
+        rmdir(self, os.path.join(self.package_folder, "lib", "cmake"))
         rmdir(self, os.path.join(self.package_folder, "lib", "pkgconfig"))
-        
-        if is_msvc(self) and self.options.shared:
-            copy(self, "*.dll", src=self.build_folder, dst=os.path.join(self.package_folder, "bin"))
-            copy(self, "*.lib", src=self.build_folder, dst=os.path.join(self.package_folder, "lib"))
-            rmdir(self, os.path.join(self.package_folder, "lib", "pkgconfig"))
+        rmdir(self, os.path.join(self.package_folder, "share"))
 
     def package_info(self):
+        self.cpp_info.set_property("cmake_file_name", "xxHash")
+        self.cpp_info.set_property("cmake_target_name", "xxHash::xxhash")
         self.cpp_info.set_property("pkg_config_name", "libxxhash")
-        
-        # Library name differs between static/shared and platforms
-        suffix = ""
-        if self.settings.os == "Windows" and not self.options.shared:
-            suffix = "_static"
-        self.cpp_info.libs = [f"xxhash{suffix}"]
-        
-        if self.settings.os in ["Linux", "FreeBSD"]:
-            self.cpp_info.system_libs = ["m"]
-        
-        # Add the utility to PATH if built
-        if self.options.utility:
-            bin_path = os.path.join(self.package_folder, "bin")
-            self.output.info(f"Appending PATH environment variable: {bin_path}")
-            self.env_info.PATH.append(bin_path)
+        # TODO: back to global scope in conan v2 once cmake_find_package_* generators removed
+        self.cpp_info.components["libxxhash"].libs = ["xxhash"]
+        self.cpp_info.components["libxxhash"].set_property("cmake_target_name", "xxHash::xxhash")
